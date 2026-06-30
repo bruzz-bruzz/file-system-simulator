6cedc2c26d5b7742e5482e9570346206809562a6
kfr <rahh28308@gmail.com>
2026-06-30 20:27:54 +0800

    a

--- FileSystem.py
+++ FileSystem.py
@@ -3,7 +3,7 @@ from collections import deque
 class FileSystem():
     def __init__(self,root):
         self.root = root
-    def bfs(self,root,returnVal,target):
+    def bfs(self,root,returnVal,target,returnArr):
         q = deque([root])
         res = []
         if not root:
@@ -12,24 +12,46 @@ class FileSystem():
             same = []
             for _ in range(len(q)):
                 p = q.popleft()
-                same.append(p if returnVal == False else p.val)
-                if p.left:
-                    q.append(p.left)
-                if p.right:
-                    q.append(p.right)
+                if p.name == target:
+                    return p
+                same.append(p if returnVal == False else p.name)
+                if p.children:
+                    for x in p.children:
+                        q.append(x)
             res.append(same)
-        return res
-    def dfs(self,root,returnVal,target):
+        return None if not returnArr else res
+    def dfs(self,root,returnVal,target,returnArr):
         stk = deque([root])
         res = []
         if not root:
             return res
         while stk:
             p = stk.popleft()
-            if p.left:
-                stk.append(p.left)
-            if p.right:
-                stk.append(p.right)
-            res.append(p if returnVal == False else p.val)
-        return p
+            if p.name == target:
+                return p
+            if p.children:
+                for x in p.children:
+                    stk.append(x)
+            res.append(p if returnVal == False else p.name)
+        return None if not returnArr else res
     def addData(self,parent,newNode):
+        parentNode = self.bfs(self.root,False,parent,False)
+        if not parentNode:
+            return f'Directory {parent} does not exist.'
+        parentNode.children.append(newNode)
+
+    def deleteData(self,parent,targetNode):
+        parentNode = self.bfs(self.root,False,parent,False)
+        found = False
+        for x in range(len(parentNode.children)):
+            if parentNode.children[x].name == targetNode:
+                found = True
+                print(f'{parentNode.children[x].type} {parentNode.children[x].name} has been deleted.')
+                parentNode.children.pop(x)
+                break
+        if not found:
+            print(f'{targetNode} does not exist in {parent}.')
+    def editData(self,targetNode):
+        node = self.bfs(self.root,False,targetNode,False)
+        if not node:
+            print(f'{targetNode} does not exist.')
--- Node.py
+++ Node.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 class Node():
-    def __init__(self,val,path,children):
-        self.val = val
+    def __init__(self,name,path,children):
+        self.name = name
         self.path = path
+        self.data = ''
         self.type = 'FILE' if '.' in val else 'FOLDER'
         self.createdDate = datetime.now()
         self.editedDate = datetime.now()
